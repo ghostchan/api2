26d75e38d6d3384187ddc6292a675f9c11e48515
ghostchan <jinnchan@qq.com>
2017-12-08 22:15:44 +0800

    列表

--- server2.js
+++ server2.js
@@ -3,10 +3,22 @@ const static=require('express-static');
 
 const bodyParser=require('body-parser');
 const consolidate=require('consolidate');
+const favicon = require('serve-favicon')
+const path = require('path')
 
+const mysql = require("mysql");
+
+//连接池
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "2008",
+  database: "api"
+});
 var server=express();
 server.listen(8080);
 
+server.use(favicon(path.join(__dirname, 'static', 'favicon.ico')))
 
 //3、post数据
 server.use(bodyParser.urlencoded({extended:false}));
@@ -25,10 +37,36 @@ server.get('/',function(req,res){
     res.render('index.ejs',{name:'chen'}); 
 });
 server.get('/list',function(req,res){
-    res.render('list.ejs',{}); 
+    db.query("SELECT * FROM params", (err, data) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send("database error")
+            .end();
+        } else {
+            res.render('list.ejs',{list:data}); 
+        }
+    });
+    
 });
 server.post('/add',function(req,res){
-    console.log(req.body.desc);
+    var desc=req.body.desc;
+    var url=req.body.apiurl;
+    var param=req.body.param;
+    console.log(desc);
+    console.log(desc.trim());
+    db.query(`INSERT INTO params VALUES("",${url},${desc},${param})`, (err, data) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send("database error")
+            .end();
+        } else {
+            console.log("添加成功！");
+        }
+    });
 });
 
 //4、static数据
